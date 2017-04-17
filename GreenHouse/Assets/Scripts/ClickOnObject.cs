1bae6812f921f2c306b3c7442182af437ead75b2
DavidFrisch <dmf463@nyu.edu>
2017-04-17 11:56:34 -0400

    set up first room, gonna start adding in objects and coding stuff

---
+++ GreenHouse/Assets/Scripts/ClickOnObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickOnObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        RaycastHit rayhit;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out rayhit))
+            {
+                Debug.Log("hitting " + rayhit.collider.gameObject.name);
+            }
+        }
+
+        
+		
+	}
+}
