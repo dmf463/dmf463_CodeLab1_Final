f1ced6a2cce6a75cb134ba69d218cab925f48cc3
DavidFrisch <dmf463@nyu.edu>
2017-04-17 15:07:33 -0400

    fine tuned object pickup

--- GreenHouse/Assets/Scripts/GameManager.cs
+++ GreenHouse/Assets/Scripts/GameManager.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
 
     bool isHolding;
+    public float distanceBetween;
+    public GameObject player;
+    const float DISTANCE_THRESHOLD = 2f;
 
 	// Use this for initialization
 	void Start () {
@@ -26,10 +29,14 @@ public class GameManager : MonoBehaviour {
         {
             if (isHolding == false && Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out rayhit))
             {
-                Debug.Log("hitting " + rayhit.collider.gameObject.name);
-                rayhit.rigidbody.isKinematic = true;
-                rayhit.transform.parent = Camera.main.transform;
-                isHolding = true;
+                distanceBetween = Vector3.Distance(player.transform.position, rayhit.transform.position);
+                if( distanceBetween < DISTANCE_THRESHOLD)
+                {
+                    Debug.Log("hitting " + rayhit.collider.gameObject.name);
+                    rayhit.rigidbody.isKinematic = true;
+                    rayhit.transform.parent = Camera.main.transform;
+                    isHolding = true;
+                }
             }
             else if (isHolding == true)
             {
