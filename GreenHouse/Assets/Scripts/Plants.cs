a879ff2aa92819458abb09857a0d6a0871848b72
DavidFrisch <dmf463@nyu.edu>
2017-04-24 22:19:30 -0400

    got the plants growing! niiiiiice, gotta add some sounds now, and then different plants

--- GreenHouse/Assets/Scripts/Plants.cs
+++ GreenHouse/Assets/Scripts/Plants.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Plants : Interactable {
+
+    GameObject plant;
 	
 	// Update is called once per frame
 	void Update () {
@@ -18,10 +20,14 @@ public class Plants : Interactable {
     public void Grow()
     {
         Debug.Log("Plant is Growing");
+        plant = transform.GetChild(0).gameObject;
+        plant.transform.position += new Vector3(0, 0.1f, 0);
     }
 
     public void Cut()
     {
         Debug.Log("Plant is Cut!");
+        plant = transform.GetChild(0).gameObject;
+        plant.transform.position -= new Vector3(0, 0.1f, 0);
     }
 }
