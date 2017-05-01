5a852361cddf15ae9cfad3d0d22e378e70aff269
DavidFrisch <dmf463@nyu.edu>
2017-04-30 22:52:26 -0400

    got the cut going!

--- GreenHouse/Assets/Scripts/Plants.cs
+++ GreenHouse/Assets/Scripts/Plants.cs
@@ -7,6 +7,7 @@ public class Plants : Interactable {
     GameObject plant;
     protected float transpose = -4;  // transpose in semitones
     protected float note = 0;
+    protected bool hasAudio = false;
     //protected AudioClip clip;
 
     public override bool IsUsable()
@@ -20,25 +21,36 @@ public class Plants : Interactable {
         plant = transform.GetChild(0).gameObject;
         plant.transform.position += new Vector3(0, 0.1f, 0);
         plant.transform.localScale += new Vector3(0, 0.15f, 0);
-        AudioSource audio = plant.AddComponent<AudioSource>();
-        audio.clip = Resources.Load("Sounds/LowNote") as AudioClip;
-        audio.loop = true;
-        Debug.Log("note + transpose = to -1? or -5 " + (note + transpose));
-        if (this.note >= 0)
+        if(hasAudio == false)
         {
-            Debug.Log("Playing Plant Note");
-            audio.pitch = Mathf.Pow(2, (note + transpose) / 12.0f);
-            audio.Play();
+            AudioSource audio = plant.AddComponent<AudioSource>();
+            audio.clip = Resources.Load("Sounds/LowNote") as AudioClip;
+            audio.loop = true;
+            Debug.Log("note + transpose = to -1? or -5 " + (note + transpose));
+            if (this.note >= 0)
+            {
+                Debug.Log("Playing Plant Note");
+                audio.pitch = Mathf.Pow(2, (note + transpose) / 12.0f);
+                audio.Play();
+                audio.volume = .1f;
+            }
+            hasAudio = true;
         }
+        else
+        {
+            AudioSource audio = plant.GetComponent<AudioSource>();
+            audio.volume += .1f;
+        }
+
     }
 
     public virtual void Cut()
     {
         Debug.Log("Plant is Cut!");
-        //plant = transform.GetChild(0).gameObject;
-        //plant.
-        this.transform.position -= new Vector3(0, 0.1f, 0);
-        //plant.
-        this.transform.localScale -= new Vector3(0, 0.15f, 0);
+        plant = transform.GetChild(0).gameObject;
+        plant.transform.position -= new Vector3(0, 0.1f, 0);
+        plant.transform.localScale -= new Vector3(0, 0.15f, 0);
+        AudioSource audio = plant.GetComponent<AudioSource>();
+        audio.volume -= .1f;
     }
 }
